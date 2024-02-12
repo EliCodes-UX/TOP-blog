af9f3128a251bd9c56297a20b7b5c5da51a4d462
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 12:49:52 -0700

    fixed header and app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,12 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} />
-      <Route index element={<Post />} />
-      <Route path={'/login'} element={<div>login</div>} />
-      <Route path={'/register'} element={<div>register</div>} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={'/login'} element={<div>login page</div>} />
+        <Route path={'/register'} element={<div>register</div>} />
+      </Route>
     </Routes>
   );
 }
-
 export default App;
