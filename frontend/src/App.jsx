7c9839a45091ed5c59b8e87d578c92245bb76215
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 11:25:07 -0700

    added more css and html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,57 @@ function App() {
           </nav>
         </header>
         <div className='entries'>
-          <h1>Articles</h1>
-          <div className='entry'></div>
+          <div className='post'>
+            <div className='image'>
+              <img src='https://avatars.githubusercontent.com/u/141448128?v=4'></img>
+            </div>
+            <div className='texts'>
+              <h2>Welcome Article</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <div className='post'>
+            <div className='image'>
+              <img src='https://avatars.githubusercontent.com/u/141448128?v=4'></img>
+            </div>
+            <div className='texts'>
+              <h2>Welcome Article</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <div className='post'>
+            <div className='image'>
+              <img src='https://avatars.githubusercontent.com/u/141448128?v=4'></img>
+            </div>
+            <div className='texts'>
+              <h2>Welcome Article</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </>
