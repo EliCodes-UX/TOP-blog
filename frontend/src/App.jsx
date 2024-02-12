c78707f41dc5bbfa32f52495159cb648190aa872
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 12:42:27 -0700

    need to fix layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css';
-import Header from './Header';
+
+import Layout from './Layout';
 import Post from './Post';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
+      <Route path='/' element={<Layout />} />
+      <Route index element={<Post />} />
+      <Route path={'/login'} element={<div>login</div>} />
+      <Route path={'/register'} element={<div>register</div>} />
     </Routes>
   );
 }
