b62a93927c28f5062edb5e06ea8b9f7948a4f44e
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 15:18:33 -0700

    added login and register page

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <form action='' className='login'>
+      <input type='text' placeholder='username'></input>
+      <input type='password' placeholder='password'></input>
+      <button>Login</button>
+    </form>
+  );
+}
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <form action='' className='register'>
+      <input type='text' placeholder='username'></input>
+      <input type='password' placeholder='password'></input>
+      <button>Register</button>
+    </form>
+  );
+}
