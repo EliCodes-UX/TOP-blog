6d80d372f342042d86891ed4a061bea1665d9d73
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 19:45:28 -0700

    added login verif

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,21 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfg2345sdfsfasdf';
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(
   'mongodb+srv://elibonner:9BUniBAXbZVEZQuG@cluster0.mgilic4.mongodb.net/?retryWrites=true&w=majority'
@@ -27,4 +37,22 @@ app.post('/register', async (req, res) => {
   }
 });
 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json('OK');
+    });
+  } else {
+    res.status(400).json('Wrong Creds');
+  }
+  res.json(passOk);
+});
+
+app.get('/profile', (req, res) => {
+  res.json(req.cookies);
+});
 app.listen(4000);
