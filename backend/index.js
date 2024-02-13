7b2f57fd175b5c8b160609a7b84b5cd319005903
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-12 17:15:57 -0700

    added ability to see fronten and backend

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
   res.json('test ok');
 });
 
